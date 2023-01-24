6fdc43a5f7838aaed3963de7c113874ed9be880d
ahmadsyafiqkamil <ahmadsyafiqkamil@gmail.com>
2023-01-24 14:49:04 +0700

    indikator 1 selesai

--- ekobistren/urls.py
+++ ekobistren/urls.py
@@ -4,12 +4,8 @@ from .views import *
 app_name = 'ekonomi'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    # path('headline_berita/', BeritaHeadlineView.as_view(), name='headline_berita'),
-    # path('get_headline_berita/', get_headline_berita, name='get_headline_berita'),
-    #
-    # path('everything_berita/', BeritaEverythingView.as_view(), name='everything_berita'),
-    # path('get_everything_berita/', get_everything_berita, name='get_everything_berita'),
-
+    path('indikator1', Indikator1View.as_view(), name='indikator1'),
+    path('indikator2', Indikator2View.as_view(), name='indikator2'),
 
     # path('notifications/', NotificationListView.as_view(), name='notifications'),
     # re_path('mark-as-read/(?P<slug>\d+)/$', mark_as_read, name='mark_as_read'),
--- ekobistren/views.py
+++ ekobistren/views.py
@@ -4,7 +4,13 @@ from django.views import generic
 
 # Create your views here.
 
-class HomeView( generic.TemplateView):
+class HomeView(generic.TemplateView):
     template_name = 'content/home.html'
 
-    
+
+class Indikator1View(generic.TemplateView):
+    template_name = 'content/indikator_1.html'
+
+
+class Indikator2View(generic.TemplateView):
+    template_name = 'content/indikator_2.html'
